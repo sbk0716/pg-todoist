9b211625afb9cd3cddbf1adc6bc838922c65e067
sbk0716 <uhey0716@gmail.com>
2022-01-16 17:49:46 +0900

    update router

--- api/infra/routers/dones.py
+++ api/infra/routers/dones.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, Request, status, Depends, Path, Header
+from typing import Optional
+from api.core.logging import logger
 from api.dependencies.db import get_repository
 from api.interfaces.db.repositories.tasks import TasksRepository
 from api.interfaces.db.repositories.dones import DonesRepository
@@ -17,13 +19,17 @@ router = APIRouter()
     status_code=status.HTTP_201_CREATED,
 )
 async def mark_task_as_done(
-    task_id: int,
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_id: int = Path(..., title="The ID of the record to get.", gt=0, le=1000),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
     dones_repo: DonesRepository = Depends(get_repository(DonesRepository)),
 ) -> DoneRead:
     """
     mark_task_as_done function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set DonesRepository and TasksRepository to DonesController instance.
     dones_controller = DonesController(tasks_repo, dones_repo)
     return await dones_controller.mark_task_as_done(task_id=task_id)
@@ -36,13 +42,17 @@ async def mark_task_as_done(
     status_code=status.HTTP_200_OK,
 )
 async def unmark_task_as_done(
-    task_id: int,
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_id: int = Path(..., title="The ID of the record to get.", gt=0, le=1000),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
     dones_repo: DonesRepository = Depends(get_repository(DonesRepository)),
 ) -> DoneRead:
     """
     unmark_task_as_done function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set DonesRepository and TasksRepository to DonesController instance.
     dones_controller = DonesController(tasks_repo, dones_repo)
     return await dones_controller.unmark_task_as_done(task_id=task_id)
--- api/infra/routers/tasks.py
+++ api/infra/routers/tasks.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, status, Depends
-from typing import List
+from fastapi import APIRouter, Request, status, Depends, Path, Header, Body
+from typing import Optional, List
+from api.core.logging import logger
 from api.dependencies.db import get_repository
 from api.interfaces.db.repositories.tasks import TasksRepository
 from api.interfaces.controllers.tasks import TasksController
@@ -20,11 +21,15 @@ router = APIRouter()
     status_code=status.HTTP_200_OK,
 )
 async def list_tasks(
+    request: Request,
+    user_agent: Optional[str] = Header(None),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> List[TaskDoneRead]:
     """
     list_tasks function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set TasksRepository to TasksController instance.
     tasks_controller = TasksController(tasks_repo)
     return await tasks_controller.list_tasks()
@@ -37,11 +42,16 @@ async def list_tasks(
     status_code=status.HTTP_200_OK,
 )
 async def get_task(
-    task_id: int, tasks_repo: TasksRepository = Depends(get_repository(TasksRepository))
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_id: int = Path(..., title="The ID of the record to get.", gt=0, le=1000),
+    tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> List[TaskDoneRead]:
     """
     get_task function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set TasksRepository to TasksController instance.
     tasks_controller = TasksController(tasks_repo)
     return await tasks_controller.get_task(task_id=task_id)
@@ -54,12 +64,16 @@ async def get_task(
     status_code=status.HTTP_201_CREATED,
 )
 async def create_task(
-    task_body: TaskCreate,
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_body: TaskCreate = Body(...),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> TaskRead:
     """
     create_task function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set TasksRepository to TasksController instance.
     tasks_controller = TasksController(tasks_repo)
     return await tasks_controller.create_task(task_body=task_body)
@@ -72,13 +86,17 @@ async def create_task(
     status_code=status.HTTP_200_OK,
 )
 async def update_task(
-    task_id: int,
-    task_body: TaskUpdate,
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_id: int = Path(..., title="The ID of the record to get.", gt=0, le=1000),
+    task_body: TaskUpdate = Body(...),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> TaskRead:
     """
     update_task function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set TasksRepository to TasksController instance.
     tasks_controller = TasksController(tasks_repo)
     return await tasks_controller.update_task(task_id=task_id, task_body=task_body)
@@ -91,12 +109,16 @@ async def update_task(
     status_code=status.HTTP_200_OK,
 )
 async def delete_task(
-    task_id: int,
+    request: Request,
+    user_agent: Optional[str] = Header(None),
+    task_id: int = Path(..., title="The ID of the record to get.", gt=0, le=1000),
     tasks_repo: TasksRepository = Depends(get_repository(TasksRepository)),
 ) -> TaskRead:
     """
     delete_task function
     """
+    logger.info(f"request.headers: {request.headers}")
+    logger.info(f"user_agent: {user_agent}")
     # Set TasksRepository to TasksController instance.
     tasks_controller = TasksController(tasks_repo)
     return await tasks_controller.delete_task(task_id=task_id)
