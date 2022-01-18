1a93675a52f9341f09a0aeaa05e36d5c81d0239d
sbk0716 <uhey0716@gmail.com>
2022-01-18 14:07:04 +0900

    add todoist schema

--- api/interfaces/db/queries/dones.py
+++ api/interfaces/db/queries/dones.py
@@ -2,25 +2,36 @@
 # CREATE
 # ====================
 CREATE_DONE_QUERY = """
-    INSERT INTO dones (id, note, created_at, updated_at)
-    VALUES (:id, :note, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-    RETURNING id, note, created_at, updated_at;
+INSERT INTO todoist.dones
+    (id, note, created_at, updated_at)
+VALUES
+    (:id, :note, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+RETURNING
+    id, note, created_at, updated_at;
 """
 
 # ====================
 # READ
 # ====================
 GET_DONE_BY_ID_QUERY = """
-    SELECT dones.id, dones.note, dones.created_at, dones.updated_at
-    FROM dones
-    WHERE id = :id;
+SELECT
+    dones.id,
+    dones.note,
+    dones.created_at,
+    dones.updated_at
+FROM
+    todoist.dones
+WHERE
+    dones.id = :id;
 """
 
 # ====================
 # DELETE
 # ====================
 DELETE_DONE_BY_ID_QUERY = """
-    DELETE FROM dones
-    WHERE id = :id
-    RETURNING id, note, created_at, updated_at;
+DELETE FROM todoist.dones
+WHERE
+    id = :id
+RETURNING
+    id, note, created_at, updated_at;
 """
